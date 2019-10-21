223530f1e6dfb81046f60abd2fe8301900b00650
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-10-21 20:04:33 +0700

    Implement button using jetpack compose

--- app/src/main/kotlin/id/kotlin/declarative/MainView.kt
+++ app/src/main/kotlin/id/kotlin/declarative/MainView.kt
@@ -1,16 +1,23 @@
 package id.kotlin.declarative
 
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.compose.Composable
+import androidx.compose.Context
 import androidx.ui.core.Text
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Center
 import androidx.ui.layout.FlexColumn
+import androidx.ui.material.Button
+import androidx.ui.material.ContainedButtonStyle
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.TopAppBar
 
 @Composable
-fun mainView(message: String) = MaterialTheme {
+fun mainView(
+    context: Context,
+    message: String
+) = MaterialTheme {
   FlexColumn {
     inflexible {
       TopAppBar<MenuItem>(
@@ -19,7 +26,13 @@ fun mainView(message: String) = MaterialTheme {
       )
     }
     Center {
-      Text("Hello, $message!")
+      Button(
+          "Click",
+          onClick = {
+            Toast.makeText(context, "Hello, $message!", Toast.LENGTH_SHORT).show()
+          },
+          style = ContainedButtonStyle(color = Color.Gray)
+      )
     }
   }
 }
